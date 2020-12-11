a29e22a4e7af8414b66a079ee5b440899e912d0d
gabrielhicks <gabrielhicks@mail.com>
2020-12-10 21:45:21 -0500

    Finding category by id

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,63 @@
 module Types
   class QueryType < Types::BaseObject
-    field :users, [Types::UserType], null: false
+    # Users Queries #
+    field :users, [Types::UserType], null: false do
+      description "Query for all Users"
+    end
 
-  def users
-    User.all
-  end
+    def users
+      User.all
+    end
+
+    # Category Queries #
+    field :categories, [Types::CategoryType], null: false do
+      description "Query for all Categories"
+    end
+
+    def categories
+      Category.all
+    end
+
+    field :find_category, [CategoryType], null: false do
+      argument :id, ID, required: true
+    end
+
+    def find_category(id:)
+      if id
+        Category.where(id: id)
+      else
+        Category.all
+      end
+    end
+    
+    # Post Queries #
+    field :posts, [Types::PostType], null: false do
+      description "Query for all Posts"
+    end
+
+    def posts
+      Post.all
+    end
+
+    # Comment Queries #
+    field :comments, [Types::CommentType], null: false do
+      description "Query for all Comments"
+    end
+
+    def comments
+      Comment.all
+    end
+
+    # Message Queries #
+    field :messages, [Types::MessageType], null: false do
+      description "Query for all Messages"
+    end
+
+    def messages
+      Message.all
+    end
+
+
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
